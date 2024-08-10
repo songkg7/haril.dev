90b493c9246aa6269339bb3c1d617b898a1e163e
Haril Song <songkg7@gmail.com>
2024-08-10 15:37:11 +0900

    ci: upgrade docusaurus version to 3.5.1 (#68)

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import BlogPostItem from "@theme-original/BlogPostItem";
 import type BlogPostItemType from "@theme/BlogPostItem";
 import type { WrapperProps } from "@docusaurus/types";
-import { useBlogPost } from "@docusaurus/theme-common/internal";
+import { useBlogPost } from "@docusaurus/plugin-content-blog/client";
 import Comments from "@site/src/components/Comments";
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
-export default function BlogPostItemWrapper(props: Props): JSX.Element {
+export default function BlogPostItemWrapper(props: Props): React.JSX.Element {
     const { metadata, isBlogPostPage } = useBlogPost();
     const { comments = true } = metadata.frontMatter;
 
