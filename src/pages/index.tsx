80b94705790ce3b128985f6cfdb193a9a4138d01
haril <songkg7@gmail.com>
2025-01-01 15:38:22 +0900

    fix(style): remove unnecessary properties

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.scss';
