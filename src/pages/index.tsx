25637b981fe88b9eff6220937e38911986c30d2b
haril song <songkg7@gmail.com>
2025-01-15 21:52:05 +0900

    style(main): fix a duplicated title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
-            title={`${siteConfig.title}`}
+            // title={`${siteConfig.title}`}
             description={`${siteConfig.tagline}`}>
             <HomepageGradient />
         </Layout>
